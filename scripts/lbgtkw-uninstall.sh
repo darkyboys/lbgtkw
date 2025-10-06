2512d65a3a31036c07de98a234cdc5262077ab8f
darkyboys <darkyboys726@gmail.com>
2025-10-06 08:17:24 +0530

    Update lbgtkw-uninstall.sh

--- scripts/lbgtkw-uninstall.sh
+++ scripts/lbgtkw-uninstall.sh
@@ -8,24 +8,24 @@ echo "Done!"
 echo ""
 
 echo "Removing the blur daemon at /usr/local/bin/lbgtkw/"
-rm -rf /usr/local/bin/lbgtkw-blur-daemon.sh
+sudo rm -rf /usr/local/bin/lbgtkw-blur-daemon.sh
 echo "Done!"
 echo ""
 
 echo "Disabling the systemd service..."
-systemctl --user disable lbgtkw-daemon-autostart.service
-systemctl --user stop lbgtkw-daemon-autostart.service
-systemctl --user daemon-reload
+sudo systemctl --user disable lbgtkw-daemon-autostart.service
+sudo systemctl --user stop lbgtkw-daemon-autostart.service
+sudo systemctl --user daemon-reload
 echo "Done!"
 echo ""
 
 echo "Removing the systemd service..."
-rm -rf ~/.config/systemd/user/lbgtkw-daemon-autostart.service
+sudo rm -rf /etc/systemd/system/lbgtkw-daemon-autostart.service
 echo "Done!"
 echo ""
 
 echo "Removing the uninstall script at /usr/local/bin/lbgtkw/"
-rm -rf /usr/local/bin/lbgtkw-uninstall.sh
+sudo rm -rf /usr/local/bin/lbgtkw-uninstall.sh
 echo "Done!"
 echo ""
 
